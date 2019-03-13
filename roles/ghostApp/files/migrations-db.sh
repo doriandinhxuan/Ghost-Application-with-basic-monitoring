0a6e6b8f51b9d40bd260f7aba4f7b2a6e7c410e2
Dorian Dinh Xuan <doriandinhxuan@MacBook-Pro-Dorian.local>
2019-03-13 17:37:38 +0100

    #############################################################################

---
+++ roles/ghostApp/files/migrations-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+username='root'
+password='ghost'
+
+mysql -h 127.0.0.1 -P 3306 -u $username --password=$password ghost-db -e "UPDATE migrations_lock set locked=0 where lock_key='km01';"
+
+exit
+
+docker restart $(docker ps -aqf "name=ghostapp")
