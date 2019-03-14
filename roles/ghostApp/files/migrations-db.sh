56de390cfcc9bd49823c1aaa139377475700383f
Dorian Dinh Xuan <doriandinhxuan@MacBook-Pro-Dorian.local>
2019-03-14 12:56:40 +0100

    Fix bugs

--- roles/ghostApp/files/migrations-db.sh
+++ roles/ghostApp/files/migrations-db.sh
@@ -3,7 +3,7 @@
 username='root'
 password='ghost'
 
-mysql -h 127.0.0.1 -P 3306 -u $username --password=$password ghost-db -e "UPDATE migrations_lock set locked=0 where lock_key='km01';"
+mysql -h 127.0.0.1 -P 3306 -u $username --password=$password ghost -e "UPDATE migrations_lock set locked=0 where lock_key='km01';"
 
 exit
 
